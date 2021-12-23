3c7fe3e929ac46850b74975df5c76863ff05ac13
Levin Noronha <levin.noro@gmail.com>
2021-12-23 17:35:15 -0500

    Fix Changefile Test

--- VF2SubIso/src/main/java/TgfdDiscovery/TestChangeFile.java
+++ VF2SubIso/src/main/java/TgfdDiscovery/TestChangeFile.java
@@ -7,12 +7,16 @@ import Infra.TGFD;
 import changeExploration.Change;
 import changeExploration.ChangeLoader;
 import graphLoader.GraphLoader;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -21,13 +25,99 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-public class TestChangeFile {
+public class TestChangeFile extends TgfdDiscovery{
+    public TestChangeFile(String[] args) {
+        super();
+        String timeAndDateStamp = ZonedDateTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("uuuu.MM.dd.HH.mm.ss"));
+        this.setExperimentDateAndTimeStamp(timeAndDateStamp);
+        this.setStartTime(System.currentTimeMillis());
+
+        Options options = TgfdDiscovery.initializeCmdOptions();
+        CommandLine cmd = TgfdDiscovery.parseArgs(options, args);
+
+        if (cmd.hasOption("path")) {
+            this.setPath(cmd.getOptionValue("path").replaceFirst("^~", System.getProperty("user.home")));
+            if (!Files.isDirectory(Path.of(this.getPath()))) {
+                System.out.println(Path.of(this.getPath()) + " is not a valid directory.");
+                return;
+            }
+            this.graphSize = Path.of(this.getPath()).getFileName().toString();
+        }
+
+        if (!cmd.hasOption("loader")) {
+            System.out.println("No specifiedLoader is specified.");
+            return;
+        } else {
+            this.setLoader(cmd.getOptionValue("loader"));
+        }
+
+        this.setUseChangeFile(this.getLoader().equalsIgnoreCase("imdb"));
+
+        if (cmd.hasOption("name")) {
+            this.setExperimentName(cmd.getOptionValue("name"));
+        } else  {
+            this.setExperimentName("experiment");
+        }
+
+        if (!cmd.hasOption("console")) {
+            PrintStream logStream = null;
+            try {
+                logStream = new PrintStream("tgfd-discovery-log-" + timeAndDateStamp + ".txt");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.setOut(logStream);
+        }
+
+        this.setOnlyInterestingTGFDs(!cmd.hasOption("uninteresting"));
+
+        this.setGeneratek0Tgfds(cmd.hasOption("k0"));
+        this.setSkipK1(cmd.hasOption("skipK1"));
+
+        this.setGamma(cmd.getOptionValue("a") == null ? TgfdDiscovery.DEFAULT_GAMMA : Integer.parseInt(cmd.getOptionValue("a")));
+        this.setTheta(cmd.getOptionValue("theta") == null ? TgfdDiscovery.DEFAULT_THETA : Double.parseDouble(cmd.getOptionValue("theta")));
+        this.setK(cmd.getOptionValue("k") == null ? TgfdDiscovery.DEFAULT_K : Integer.parseInt(cmd.getOptionValue("k")));
+        this.setFrequentSetSize(cmd.getOptionValue("p") == null ? TgfdDiscovery.DEFAULT_FREQUENT_SIZE_SET : Integer.parseInt(cmd.getOptionValue("p")));
+
+        this.initializeTgfdLists();
+
+        if (cmd.hasOption("K")) this.markAsKexperiment();
+
+        this.setKeepSuperVertex(cmd.hasOption("keepSuperVertex"));
+
+        switch (this.getLoader().toLowerCase()) {
+            case "dbpedia" -> this.setDBpediaTimestampsAndFilePaths(this.getPath());
+            case "citation" -> this.setCitationTimestampsAndFilePaths(this.getPath());
+            case "imdb" -> this.setImdbTimestampToFilesMapFromPath(this.getPath());
+            default -> {
+                System.out.println("No loader is specified.");
+                System.exit(1);
+            }
+        }
+        this.loadGraphsAndComputeHistogram(this.getTimestampToFilesMap().subList(0,1));
+//        this.loadChangeFilesIntoMemory();
+
+        String[] info = {
+                String.join("=", "loader", this.graphSize),
+                String.join("=", "|G|", this.graphSize),
+                String.join("=", "k", Integer.toString(this.getK())),
+                String.join("=", "theta", Double.toString(this.getTheta())),
+                String.join("=", "gamma", Double.toString(this.getGamma())),
+                String.join("=", "frequentSetSize", Double.toString(this.getFrequentSetSize())),
+                String.join("=", "interesting", Boolean.toString(this.isOnlyInterestingTGFDs())),
+                String.join("=", "noMinimalityPruning", Boolean.toString(!this.hasMinimalityPruning())),
+                String.join("=", "noSupportPruning", Boolean.toString(!this.hasSupportPruning())),
+        };
+
+        System.out.println(String.join(", ", info));
+    }
+
     public static void main(String[] args) {
         String overallTimeAndDateStamp = ZonedDateTime.now(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("uuuu.MM.dd.HH.mm.ss"));
         ArrayList<Long> runtimes = new ArrayList<>();
         for (int index = 0; index <= 4; index++) {
 
-            TgfdDiscovery tgfdDiscovery = new TgfdDiscovery(args);
+            TestChangeFile tgfdDiscovery = new TestChangeFile(args);
 
             tgfdDiscovery.setUseChangeFile(false);
             tgfdDiscovery.setReUseMatches(false);
@@ -43,28 +133,11 @@ public class TestChangeFile {
                 tgfdDiscovery.setMinimalityPruning(true);
             }
 
-            List<GraphLoader> graphs;
-            if (!tgfdDiscovery.useChangeFile()) {
-                tgfdDiscovery.setUseChangeFile(true);
-                graphs = tgfdDiscovery.loadDBpediaSnapshotsFromPath(tgfdDiscovery.getPath());
-                tgfdDiscovery.setUseChangeFile(false);
-            } else {
-                graphs = tgfdDiscovery.loadDBpediaSnapshotsFromPath(tgfdDiscovery.getPath());
-            }
-
-            // Had to compute histogram using only first snapshot because we need dummy TGFDs to build the other graphs
-            // TO-DO: Is there a better way?
-            if (tgfdDiscovery.useChangeFile()) {
-                tgfdDiscovery.histogram(tgfdDiscovery.getTimestampToFilesMap().subList(0,1));
-            } else {
-                tgfdDiscovery.histogram(graphs);
-            }
-
-            if (graphs.size() == 1) {
+            if (tgfdDiscovery.getGraphs().size() == 1) {
+                HashMap<String, org.json.simple.JSONArray> changeFilesMap = new HashMap<>();
                 for (int i = 0; i < 2; i++) {
                     List<HashMap<Integer, HashSet<Change>>> changes = new ArrayList<>();
                     String changefilePath = "changes_t" + (i + 1) + "_t" + (i + 2) + "_" + tgfdDiscovery.graphSize + ".json";
-                    HashMap<String, org.json.simple.JSONArray> changeFilesMap = new HashMap<>();
                     JSONParser parser = new JSONParser();
                     Object json;
                     org.json.simple.JSONArray jsonArray = new JSONArray();
@@ -81,7 +154,7 @@ public class TestChangeFile {
                     System.out.println("Total number of changes in changefile: " + newChanges.size());
                     changes.add(newChanges);
                     ArrayList<TGFD> tgfds = tgfdDiscovery.getDummyTGFDs();
-                    IncUpdates incUpdatesOnDBpedia = new IncUpdates(graphs.get(0).getGraph(), tgfds);
+                    IncUpdates incUpdatesOnDBpedia = new IncUpdates(tgfdDiscovery.getGraphs().get(0).getGraph(), tgfds);
                     incUpdatesOnDBpedia.AddNewVertices(changeLoader.getAllChanges());
                     HashMap<String, TGFD> tgfdsByName = new HashMap<>();
                     for (TGFD tgfd : tgfds) {
@@ -94,11 +167,13 @@ public class TestChangeFile {
                     }
                     GraphLoader graphLoader = new GraphLoader();
                     graphLoader.setGraph(incUpdatesOnDBpedia.getBaseGraph());
-                    graphs.add(graphLoader);
+                    tgfdDiscovery.getGraphs().add(graphLoader);
                 }
+                tgfdDiscovery.setChangeFilesMap(changeFilesMap);
             }
+            tgfdDiscovery.setNumOfSnapshots(tgfdDiscovery.getGraphs().size());
 
-            tgfdDiscovery.initialize(graphs);
+            tgfdDiscovery.initialize(tgfdDiscovery.getGraphs());
 
             while (tgfdDiscovery.getCurrentVSpawnLevel() <= tgfdDiscovery.getK()) {
 
@@ -123,19 +198,19 @@ public class TestChangeFile {
 
                 assert patternTreeNode != null;
                 if (tgfdDiscovery.isValidationSearch()) {
-                    tgfdDiscovery.getMatchesForPattern(graphs, patternTreeNode, matches);
+                    tgfdDiscovery.getMatchesForPattern(tgfdDiscovery.getGraphs(), patternTreeNode, matches);
                     matchingTime = System.currentTimeMillis() - matchingTime;
-                    TgfdDiscovery.printWithTime("getMatchesUsingChangefiles", (matchingTime));
+                    TgfdDiscovery.printWithTime("getMatchesUsingChangeFiles", (matchingTime));
                     tgfdDiscovery.addToTotalMatchingTime(matchingTime);
                 }
                 else if (tgfdDiscovery.useChangeFile()) {
-                    tgfdDiscovery.getMatchesUsingChangefiles(graphs, patternTreeNode, matches);
+                    tgfdDiscovery.getMatchesUsingChangeFiles(tgfdDiscovery.getGraphs(), patternTreeNode, matches);
                     matchingTime = System.currentTimeMillis() - matchingTime;
-                    TgfdDiscovery.printWithTime("getMatchesUsingChangefiles", (matchingTime));
+                    TgfdDiscovery.printWithTime("getMatchesUsingChangeFiles", (matchingTime));
                     tgfdDiscovery.addToTotalMatchingTime(matchingTime);
                 }
                 else {
-                    tgfdDiscovery.findMatchesUsingCenterVertices(graphs, patternTreeNode, matches);
+                    tgfdDiscovery.findMatchesUsingCenterVertices(tgfdDiscovery.getGraphs(), patternTreeNode, matches);
                     matchingTime = System.currentTimeMillis() - matchingTime;
                     TgfdDiscovery.printWithTime("findMatchesUsingCenterVertices", (matchingTime));
                     tgfdDiscovery.addToTotalMatchingTime(matchingTime);
