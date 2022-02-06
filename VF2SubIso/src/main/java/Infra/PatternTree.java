ff73eb63ebccff503cd5307864a6debdcab40f32
Levin Noronha <levin.noro@gmail.com>
2022-02-06 13:41:26 -0500

    Add checks to increase chances of finding centerVertexParent for pattern

--- VF2SubIso/src/main/java/Infra/PatternTree.java
+++ VF2SubIso/src/main/java/Infra/PatternTree.java
@@ -1,22 +1,23 @@
 package Infra;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PatternTree {
-    private ArrayList<ArrayList<PatternTreeNode>> tree;
+    public ArrayList<ArrayList<PatternTreeNode>> tree;
 
     public PatternTree() {
-        tree = new ArrayList<>();
+        setTree(new ArrayList<>());
     }
 
     public void addLevel() {
-        tree.add(new ArrayList<>());
-        System.out.println("GenerationTree levels: " + tree.size());
+        getTree().add(new ArrayList<>());
+        System.out.println("GenerationTree levels: " + getTree().size());
     }
 
     public PatternTreeNode createNodeAtLevel(int level, VF2PatternGraph pattern, PatternTreeNode parentNode, String candidateEdgeString) {
         PatternTreeNode node = new PatternTreeNode(pattern, parentNode, candidateEdgeString);
-        tree.get(level).add(node);
+        getTree().get(level).add(node);
         findSubgraphParents(level-1, node);
         findCenterVertexParent(level-1, node); // TO-DO: Why do we only need one parent?
         return node;
@@ -24,7 +25,7 @@ public class PatternTree {
 
     public PatternTreeNode createNodeAtLevel(int level, VF2PatternGraph pattern) {
         PatternTreeNode node = new PatternTreeNode(pattern);
-        tree.get(level).add(node);
+        getTree().get(level).add(node);
         return node;
     }
 
@@ -35,7 +36,7 @@ public class PatternTree {
             System.out.println("Finding subgraph parents...");
             ArrayList<String> newPatternVertices = new ArrayList<>();
             node.getGraph().vertexSet().forEach((vertex) -> {newPatternVertices.add(vertex.getTypes().iterator().next());});
-            for (PatternTreeNode otherPatternNode : this.tree.get(level)) {
+            for (PatternTreeNode otherPatternNode : this.getTree().get(level)) {
                 if (newPatternVertices.containsAll(otherPatternNode.getGraph().vertexSet().iterator().next().getTypes())) {
                     System.out.println("New pattern: " + node.getPattern());
                     System.out.println("is a child of subgraph  parent pattern: " + otherPatternNode.getGraph().vertexSet());
@@ -46,7 +47,7 @@ public class PatternTree {
         }
         ArrayList<String> newPatternEdges = new ArrayList<>();
         node.getGraph().edgeSet().forEach((edge) -> {newPatternEdges.add(edge.toString());});
-        for (PatternTreeNode otherPatternNode : this.tree.get(level)) {
+        for (PatternTreeNode otherPatternNode : this.getTree().get(level)) {
             ArrayList<String> otherPatternEdges = new ArrayList<>();
             otherPatternNode.getGraph().edgeSet().forEach((edge) -> {otherPatternEdges.add(edge.toString());});
             if (newPatternEdges.containsAll(otherPatternEdges)) { // TO-DO: Should we also check for center vertex equality here?
@@ -66,12 +67,14 @@ public class PatternTree {
         System.out.println("Finding center vertex parent...");
         ArrayList<String> newPatternEdges = new ArrayList<>();
         node.getGraph().edgeSet().forEach((edge) -> {newPatternEdges.add(edge.toString());});
-        for (PatternTreeNode otherPatternNode : this.tree.get(level)) {
+        List<PatternTreeNode> almostParents = new ArrayList<>();
+        for (PatternTreeNode otherPatternNode : this.getTree().get(level)) {
             ArrayList<String> otherPatternEdges = new ArrayList<>();
             otherPatternNode.getGraph().edgeSet().forEach((edge) -> {otherPatternEdges.add(edge.toString());});
             if (newPatternEdges.containsAll(otherPatternEdges)) {
-                System.out.println("New pattern: " + node.getPattern());
+                almostParents.add(otherPatternNode);
                 if (otherPatternNode.getPattern().getCenterVertexType().equals(node.getPattern().getCenterVertexType())) {
+                    System.out.println("New pattern: " + node.getPattern());
                     if (otherPatternNode.getGraph().edgeSet().size() == 0) {
                         System.out.println("is a child of center vertex parent pattern: " + otherPatternNode.getGraph().vertexSet());
                     } else {
@@ -82,14 +85,53 @@ public class PatternTree {
                 }
             }
         }
+        if (node.getCenterVertexParent() == null) {
+            for (PatternTreeNode almostParent: almostParents) {
+                for (Vertex v: node.getGraph().vertexSet()) {
+                    String almostParentCenterVertexType = almostParent.getPattern().getCenterVertexType();
+                    if (v.getTypes().contains(almostParentCenterVertexType)) {
+                        if (node.getPattern().calculateRadiusForGivenVertex(v) == node.getPattern().getRadius()) {
+                            node.getPattern().setCenterVertexType(almostParentCenterVertexType);
+                            System.out.println("New pattern: " + node.getPattern());
+                            if (almostParent.getGraph().edgeSet().size() == 0) {
+                                System.out.println("is a child of center vertex parent pattern: " + almostParent.getGraph().vertexSet());
+                            } else {
+                                System.out.println("is a child of center vertex parent pattern: " + almostParent.getPattern());
+                            }
+                            node.setCenterVertexParent(almostParent);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if (node.getCenterVertexParent() == null) {
+            for (PatternTreeNode otherPatternNode : this.getTree().get(0)) {
+                if (otherPatternNode.getPattern().getCenterVertexType().equals(node.getPattern().getCenterVertexType())) {
+                    System.out.println("New pattern: " + node.getPattern());
+                    if (otherPatternNode.getGraph().edgeSet().size() == 0) {
+                        System.out.println("is a child of center vertex parent pattern: " + otherPatternNode.getGraph().vertexSet());
+                    }
+                    node.setCenterVertexParent(otherPatternNode);
+                    return;
+                }
+            }
+        }
     }
 
     public ArrayList<PatternTreeNode> getLevel(int i) {
-        return this.tree.get(i);
+        return this.getTree().get(i);
     }
 
     public int getNumOfTreeLevels() {
-        return this.tree.size();
+        return this.getTree().size();
     }
 
+    public ArrayList<ArrayList<PatternTreeNode>> getTree() {
+        return tree;
+    }
+
+    public void setTree(ArrayList<ArrayList<PatternTreeNode>> tree) {
+        this.tree = tree;
+    }
 }
