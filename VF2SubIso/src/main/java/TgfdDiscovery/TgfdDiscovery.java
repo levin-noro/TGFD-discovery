68cefefa68740729b06f1f47d83c17886d8a271e
Levin Noronha <levin.noro@gmail.com>
2022-03-11 02:53:48 -0500

    Use short list of counters instead of long list of duplicate timestamps when finding deltas for constant TGFDs.

--- VF2SubIso/src/main/java/TgfdDiscovery/TgfdDiscovery.java
+++ VF2SubIso/src/main/java/TgfdDiscovery/TgfdDiscovery.java
@@ -1806,45 +1806,20 @@ public class TgfdDiscovery {
 			System.out.println("Entity: " + newDependency);
 
 			System.out.println("Candidate RHS values for entity...");
-			ArrayList<Entry<ConstantLiteral, List<Integer>>> attrValuesTimestampsSortedByFreq = entityEntry.getValue();
-			for (Map.Entry<ConstantLiteral, List<Integer>> entry : attrValuesTimestampsSortedByFreq) {
+			ArrayList<Entry<ConstantLiteral, List<Integer>>> rhsAttrValuesTimestampsSortedByFreq = entityEntry.getValue();
+			for (Map.Entry<ConstantLiteral, List<Integer>> entry : rhsAttrValuesTimestampsSortedByFreq) {
 				System.out.println(entry.getKey() + ":" + entry.getValue());
 			}
 
-            System.out.println("Computing candidate delta for RHS value...\n" + attrValuesTimestampsSortedByFreq.get(0).getKey());
+            System.out.println("Computing candidate delta for RHS value...\n" + rhsAttrValuesTimestampsSortedByFreq.get(0).getKey());
             ArrayList<Pair> candidateDeltas = new ArrayList<>();
-            if (attrValuesTimestampsSortedByFreq.size() == 1) {
-				List<Integer> timestamps = attrValuesTimestampsSortedByFreq.get(0).getValue();
-				int minDistance = this.getNumOfSnapshots() - 1;
-				int maxDistance = timestamps.get(timestamps.size() - 1) - timestamps.get(0);
-				for (int index = 1; index < timestamps.size(); index++) {
-					minDistance = Math.min(minDistance, timestamps.get(index) - timestamps.get(index - 1));
-				}
-				if (minDistance > maxDistance) {
-					System.out.println("Not enough timestamped matches found for entity.");
-					continue;
-				}
-				candidateDeltas.add(new Pair(minDistance, maxDistance));
-			} else if (attrValuesTimestampsSortedByFreq.size() > 1) {
-				List<Integer> mostFreqTimestamps = attrValuesTimestampsSortedByFreq.get(0).getValue();
-				int minExclusionDistance = this.getNumOfSnapshots() - 1;
-				int maxExclusionDistance = 0;
-				for (Map.Entry<ConstantLiteral, List<Integer>> otherTimestamps : attrValuesTimestampsSortedByFreq.subList(1,attrValuesTimestampsSortedByFreq.size())) {
-					for (Integer timestamp: otherTimestamps.getValue()) {
-						for (Integer refTimestamp: mostFreqTimestamps) {
-							int distance = Math.abs(timestamp - refTimestamp);
-							minExclusionDistance = Math.min(minExclusionDistance, distance);
-							maxExclusionDistance = Math.max(maxExclusionDistance, distance);
-						}
-					}
-					if (minExclusionDistance == 0 && maxExclusionDistance == (this.getNumOfSnapshots() -1)) break;
-				}
-				if (minExclusionDistance > 0) {
-					candidateDeltas.add(new Pair(0, minExclusionDistance-1));
-				}
-				if (maxExclusionDistance < this.getNumOfSnapshots() -1) {
-					candidateDeltas.add(new Pair(maxExclusionDistance+1, this.getNumOfSnapshots() -1));
-				}
+            if (rhsAttrValuesTimestampsSortedByFreq.size() == 1) {
+				List<Integer> timestampCounts = rhsAttrValuesTimestampsSortedByFreq.get(0).getValue();
+				Pair candidateDelta = getMinMaxPair(timestampCounts);
+				if (candidateDelta == null) continue;
+				candidateDeltas.add(candidateDelta);
+			} else if (rhsAttrValuesTimestampsSortedByFreq.size() > 1) {
+				findCandidateDeltasForMostFreqRHS(rhsAttrValuesTimestampsSortedByFreq, candidateDeltas);
 			}
 			if (candidateDeltas.size() == 0) {
 				System.out.println("Could not find any deltas for entity: " + entityEntry.getKey());
@@ -1862,12 +1837,18 @@ public class TgfdDiscovery {
 				if (minDistance <= maxDistance) {
 					System.out.println("Calculating support for candidate delta ("+minDistance+","+maxDistance+")");
 					double numerator;
-					List<Integer> timestamps = attrValuesTimestampsSortedByFreq.get(0).getValue();
+					List<Integer> timestampCounts = rhsAttrValuesTimestampsSortedByFreq.get(0).getValue();
 					TreeSet<Pair> satisfyingPairs = new TreeSet<>();
-					for (int index = 0; index < timestamps.size() - 1; index++) {
-						for (int j = index + 1; j < timestamps.size(); j++) {
-							if (timestamps.get(j) - timestamps.get(index) >= minDistance && timestamps.get(j) - timestamps.get(index) <= maxDistance) {
-								satisfyingPairs.add(new Pair(timestamps.get(index), timestamps.get(j)));
+					for (int index = 0; index < timestampCounts.size(); index++) {
+						if (timestampCounts.get(index) == 0)
+							continue;
+						else if (timestampCounts.get(index) > 1 && 0 >= minDistance && 0 <= maxDistance)
+							satisfyingPairs.add(new Pair(index, index));
+						for (int j = index + 1; j < timestampCounts.size(); j++) {
+							if (timestampCounts.get(j) > 0) {
+								if (j - index >= minDistance && j - index <= maxDistance) {
+									satisfyingPairs.add(new Pair(index, j));
+								}
 							}
 						}
 					}
@@ -1925,6 +1906,88 @@ public class TgfdDiscovery {
 			}
 		}
 		return tgfds;
+	}
+
+	private void findCandidateDeltasForMostFreqRHS(ArrayList<Entry<ConstantLiteral, List<Integer>>> rhsAttrValuesTimestampsSortedByFreq, ArrayList<Pair> candidateDeltas) {
+		List<Integer> timestampCountOfMostFreqRHS = rhsAttrValuesTimestampsSortedByFreq.get(0).getValue();
+		Integer minExclusionDistance = null;
+		Integer maxExclusionDistance = null;
+		for (Entry<ConstantLiteral, List<Integer>> timestampCountEntryOfOtherRHS : rhsAttrValuesTimestampsSortedByFreq.subList(1, rhsAttrValuesTimestampsSortedByFreq.size())) {
+			List<Integer> timestampCountOfOtherRHS = timestampCountEntryOfOtherRHS.getValue();
+			for (int i = 0; i < timestampCountOfOtherRHS.size(); i++) {
+				int otherTimestampCount = timestampCountOfOtherRHS.get(i);
+				if (otherTimestampCount == 0)
+					continue;
+				for (int j = 0; j < timestampCountOfMostFreqRHS.size(); j++) {
+					Integer refTimestampCount = timestampCountOfMostFreqRHS.get(j);
+					if (refTimestampCount == 0)
+						continue;
+					int distance = Math.abs(i - j);
+					minExclusionDistance = minExclusionDistance != null ? Math.min(minExclusionDistance, distance) : distance;
+					maxExclusionDistance = maxExclusionDistance != null ? Math.max(maxExclusionDistance, distance) : distance;
+				}
+			}
+			if (minExclusionDistance != null && minExclusionDistance == 0
+					&& maxExclusionDistance != null && maxExclusionDistance == (this.getNumOfSnapshots()-1))
+				break;
+		}
+		if (minExclusionDistance != null && minExclusionDistance > 0) {
+			candidateDeltas.add(new Pair(0, minExclusionDistance-1));
+		}
+		if (maxExclusionDistance != null && maxExclusionDistance < this.getNumOfSnapshots()-1) {
+			candidateDeltas.add(new Pair(maxExclusionDistance+1, this.getNumOfSnapshots()-1));
+		}
+	}
+
+	@Nullable
+	private Pair getMinMaxPair(List<Integer> timestampCounts) {
+		Integer minDistance = null;
+		Integer maxDistance = null;
+		if (timestampCounts.stream().anyMatch(count -> count > 1)) {
+			minDistance = 0;
+		} else {
+			Integer indexOfPreviousOccurence = null;
+			for (int index = 0; index < timestampCounts.size(); index++) {
+				if (timestampCounts.get(index) > 0) {
+					if (indexOfPreviousOccurence == null) {
+						indexOfPreviousOccurence = index;
+					} else {
+						minDistance = minDistance != null ? Math.min(minDistance, index - indexOfPreviousOccurence) : (index - indexOfPreviousOccurence);
+						if (minDistance == 0) break;
+					}
+				}
+			}
+		}
+		if (minDistance == null)
+			return null;
+		Integer indexOfFirstOccurence = null;
+		for (int index = 0; index < timestampCounts.size(); index++) {
+			if (timestampCounts.get(index) > 0) {
+				indexOfFirstOccurence = index;
+				break;
+			}
+		}
+		Integer indexOfFinalOccurence = null;
+		for (int index = timestampCounts.size()-1; index >= 0; index--) {
+			if (timestampCounts.get(index) > 0) {
+				indexOfFinalOccurence = index;
+				break;
+			}
+		}
+		if (indexOfFirstOccurence != null && indexOfFinalOccurence != null) {
+			if (indexOfFirstOccurence.equals(indexOfFinalOccurence) && timestampCounts.get(indexOfFirstOccurence) > 1) {
+				maxDistance = 0;
+			} else {
+				maxDistance = indexOfFinalOccurence - indexOfFirstOccurence;
+			}
+		}
+		if (maxDistance == null)
+			return null;
+		if (minDistance > maxDistance) {
+			System.out.println("Not enough timestamped matches found for entity.");
+			return null;
+		}
+		return new Pair(minDistance, maxDistance);
 	}
 
 	public ArrayList<TGFD> getDummyVertexTypeTGFDs() {
