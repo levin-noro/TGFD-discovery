490010f7bab3692b6b12538008bcba2e3956e1e7
Levin Noronha <levin.noro@gmail.com>
2022-03-11 02:56:10 -0500

    Update equals() and hashcode() for AttributeDependency and Delta

--- VF2SubIso/src/main/java/Infra/Delta.java
+++ VF2SubIso/src/main/java/Infra/Delta.java
@@ -2,12 +2,13 @@ package Infra;
 
 import java.time.Duration;
 import java.time.Period;
+import java.util.Objects;
 
 public class Delta {
 
-    private Period min;
-    private Period max;
-    private Duration granularity;
+    private final Period min;
+    private final Period max;
+    private final Duration granularity;
 
     public Delta(Period min, Period max, Duration granularity)
     {
@@ -40,5 +41,18 @@ public class Delta {
 
     public boolean subsetOf(Delta delta) {
         return this.getMin().getDays() >= delta.getMin().getDays() && this.getMax().getDays() <= delta.getMax().getDays();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Delta delta = (Delta) o;
+        return min.getDays() == delta.min.getDays() && max.getDays() == delta.max.getDays() && granularity.toDays() == delta.granularity.toDays();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(min, max, granularity);
     }
 }
